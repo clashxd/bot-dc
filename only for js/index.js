05dd418dda689a21bf42f8684995ba55559f7f8a
clashxd <hammadalam72@yahoo.com>
2024-07-21 17:28:34 +0530

    Add files via upload

---
+++ only for js/index.js
@@ -0,0 +1,19 @@
+// nn ss bb u
+console.log("clash")
+let a = null;
+let b = 69;
+let c = "clash"
+let d = Symbol("i am a nice symbol")
+let e = true //can also be false
+let f = BigInt ('-345')
+let g 
+console.log(a, b, c, d, e, f, g, )
+console.log(typeof c)
+
+// Object in js
+const item = {
+   "clash": true,
+   "hammad": false,
+   "gian": 67,
+   "dev": undefined
+}
